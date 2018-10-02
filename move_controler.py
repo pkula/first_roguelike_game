5a1e3d7ed068dccaf4b66afdd1bef94aacb7d690
Lucy <px.kula@gmail.com>
2018-10-02 14:37:07 +0200

    refactor move

--- move_controler.py
+++ move_controler.py
@@ -1,35 +1,29 @@
-def move(table,input):
+import os
+import view
+
+
+
+def move(table,input,x,y):
     if input == 's' and table[y+1][x] != '#':
-        os.system("clear")
         table[y] = view.replace_in_string(table[y],x,'.')
         table[y+1] = view.replace_in_string(table[y+1],x,'@')
         y = y + 1
-        view.print_table(table)
-
     if input == 'w' and table[y-1][x] != '#':
-        os.system("clear")
         table[y] = view.replace_in_string(table[y],x,'.')
         table[y-1] = view.replace_in_string(table[y-1],x,'@')
         y = y - 1
-        view.print_table(table)
-
     if input == 'd' and table[y][x+1] != '#':
-        os.system("clear")
         table[y] = view.replace_in_string(table[y],x,'.')
         table[y] = view.replace_in_string(table[y],x+1,'@')
         x = x + 1
-        view.print_table(table)
-
-
     if input == 'a' and table[y][x-1] != '#':
-        os.system("clear")
         table[y] = view.replace_in_string(table[y],x,'.')
         table[y] = view.replace_in_string(table[y],x-1,'@')
         x = x - 1
-        view.print_table(table)
-
-
+    o = [table,x,y]
+    return o
 
 
 
 def teleport():
+    pass
--- root_controler.py
+++ root_controler.py
@@ -13,36 +13,15 @@ def getch():
     return ch
 x = 3
 y = 3
-coordinates = [x,y]
+#coordinates = [x,y]
 view.print_table(view.get_table_from_file("table.txt"))
 table = view.get_table_from_file("table.txt")
 while True:
     input = getch()
-    if input == 's' and table[y+1][x] != '#':
-        os.system("clear")
-        table[y] = view.replace_in_string(table[y],x,'.')
-        table[y+1] = view.replace_in_string(table[y+1],x,'@')
-        y = y + 1
-        view.print_table(table)
-
-    if input == 'w' and table[y-1][x] != '#':
-        os.system("clear")
-        table[y] = view.replace_in_string(table[y],x,'.')
-        table[y-1] = view.replace_in_string(table[y-1],x,'@')
-        y = y - 1
-        view.print_table(table)
-
-    if input == 'd' and table[y][x+1] != '#':
-        os.system("clear")
-        table[y] = view.replace_in_string(table[y],x,'.')
-        table[y] = view.replace_in_string(table[y],x+1,'@')
-        x = x + 1
-        view.print_table(table)
-
-
-    if input == 'a' and table[y][x-1] != '#':
-        os.system("clear")
-        table[y] = view.replace_in_string(table[y],x,'.')
-        table[y] = view.replace_in_string(table[y],x-1,'@')
-        x = x - 1
-        view.print_table(table)
+    os.system("clear")
+    w = move.move(table,input,x,y)
+    table = w[0]
+    x = w[1]
+    y = w[2]
+    #k = move.teleport(table,input,x,y)
+    view.print_table(table)
