d85b2579bfe7a34cc2181f1493de6c32a2aaa9a2
Liz Ch <pachaffy@gmail.com>
2018-10-24 11:45:26 +0200

    pep 8 has been corrected in data_manager, main, view, main and root controller

--- move_controler.py
+++ move_controler.py
@@ -2,77 +2,81 @@ import os
 import view
 import data_manager as data
 
-def moves(table,input,x,y):
+
+def moves(table, input, x, y):
     if input == 's' and table[y+1][x] != '#':
-        table[y] = data.replace_in_string(table[y],x,'.')
-        table[y+1] = data.replace_in_string(table[y+1],x,'@')
+        table[y] = data.replace_in_string(table[y], x, '.')
+        table[y+1] = data.replace_in_string(table[y+1], x, '@')
         y = y + 1
     if input == 'w' and table[y-1][x] != '#':
-        table[y] = data.replace_in_string(table[y],x,'.')
-        table[y-1] = data.replace_in_string(table[y-1],x,'@')
+        table[y] = data.replace_in_string(table[y], x, '.')
+        table[y-1] = data.replace_in_string(table[y-1], x, '@')
         y = y - 1
     if input == 'd' and table[y][x+1] != '#':
-        table[y] = data.replace_in_string(table[y],x,'.')
-        table[y] = data.replace_in_string(table[y],x+1,'@')
+        table[y] = data.replace_in_string(table[y], x, '.')
+        table[y] = data.replace_in_string(table[y], x+1, '@')
         x = x + 1
     if input == 'a' and table[y][x-1] != '#':
-        table[y] = data.replace_in_string(table[y],x,'.')
-        table[y] = data.replace_in_string(table[y],x-1,'@')
+        table[y] = data.replace_in_string(table[y], x, '.')
+        table[y] = data.replace_in_string(table[y], x-1, '@')
         x = x - 1
-    o = [table,x,y]
+    o = [table, x, y]
     return o
 
-def teleport(table,input,x,y,cover_table):
+
+def teleport(table, input, x, y, cover_table):
     if input == 't':
         view.print_table(cover_table)
         input = data.getch()
         if input == 's' and table[y+2][x] != '#':
-            table[y] = data.replace_in_string(table[y],x,'.')
-            cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y+2] = data.replace_in_string(table[y+2],x,'@')
+            table[y] = data.replace_in_string(table[y], x, '.')
+            cover_table[y] = data.replace_in_string(cover_table[y], x, '.')
+            table[y+2] = data.replace_in_string(table[y+2], x, '@')
             y = y + 2
         if input == 'w' and table[y-2][x] != '#':
-            table[y] = data.replace_in_string(table[y],x,'.')
-            cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y-2] = data.replace_in_string(table[y-2],x,'@')
+            table[y] = data.replace_in_string(table[y], x, '.')
+            cover_table[y] = data.replace_in_string(cover_table[y], x, '.')
+            table[y-2] = data.replace_in_string(table[y-2], x, '@')
             y = y - 2
         if input == 'd' and table[y][x+2] != '#':
-            table[y] = data.replace_in_string(table[y],x,'.')
-            cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y] = data.replace_in_string(table[y],x+2,'@')
+            table[y] = data.replace_in_string(table[y], x, '.')
+            cover_table[y] = data.replace_in_string(cover_table[y], x, '.')
+            table[y] = data.replace_in_string(table[y], x+2, '@')
             x = x + 2
         if input == 'a' and table[y][x-2] != '#':
-            table[y] = data.replace_in_string(table[y],x,'.')
-            cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y] = data.replace_in_string(table[y],x-2,'@')
+            table[y] = data.replace_in_string(table[y], x, '.')
+            cover_table[y] = data.replace_in_string(cover_table[y], x, '.')
+            table[y] = data.replace_in_string(table[y], x-2, '@')
             x = x - 2
-    o = [table,x,y]
+    o = [table, x, y]
     os.system("clear")
     return o
 
-def gate_teleport(table,cover_table,coordinates_a_b,x,y):
+
+def gate_teleport(table, cover_table, coordinates_a_b, x, y):
     if coordinates_a_b[0] == x and coordinates_a_b[1] == y:
-        table[y] = data.replace_in_string(table[y],x,'a')
-        cover_table[y] = data.replace_in_string(cover_table[y],x,'a')
+        table[y] = data.replace_in_string(table[y], x, 'a')
+        cover_table[y] = data.replace_in_string(cover_table[y], x, 'a')
         x = coordinates_a_b[2]
         y = coordinates_a_b[3]+1
-        table[y] = data.replace_in_string(table[y],x,'@')
+        table[y] = data.replace_in_string(table[y], x, '@')
     if coordinates_a_b[2] == x and coordinates_a_b[3] == y:
-        table[y] = data.replace_in_string(table[y],x,'b')
-        cover_table[y] = data.replace_in_string(cover_table[y],x,'b')
+        table[y] = data.replace_in_string(table[y], x, 'b')
+        cover_table[y] = data.replace_in_string(cover_table[y], x, 'b')
         x = coordinates_a_b[0]
         y = coordinates_a_b[1]-1
-        table[y] = data.replace_in_string(table[y],x,'@')
-    l = [x,y,table]
+        table[y] = data.replace_in_string(table[y], x, '@')
+    l = [x, y, table]
     return l
 
 
-def water(coordinates_water,x,y):
+def water(coordinates_water, x, y):
     for i in coordinates_water:
         if i[0] == x and i[1] == y:
             exit()
 
-def dolars(coordinates_dolars,x,y):
+
+def dolars(coordinates_dolars, x, y):
     coordinates = []
     for i in coordinates_dolars:
         if i[0] == x and i[1] == y:
--- root_controler.py
+++ root_controler.py
@@ -3,7 +3,8 @@ import os
 import move_controler as moves
 import data_manager as data
 
-#coordinates = [x,y]
+
+# coordinates = [x,y]
 def run():
     x = 3
     y = 3
@@ -16,23 +17,23 @@ def run():
     while True:
         input = data.getch()
         os.system("clear")
-        w = moves.moves(table,input,x,y)
+        w = moves.moves(table, input, x, y)
         table = w[0]
         x = w[1]
         y = w[2]
-        data.discover_table(table,cover_table,x,y)
-        w = moves.teleport(table,input,x,y,cover_table)
+        data.discover_table(table, cover_table, x, y)
+        w = moves.teleport(table, input, x, y, cover_table)
         table = w[0]
         x = w[1]
         y = w[2]
-        data.discover_table(table,cover_table,x,y)
-        w = moves.gate_teleport(table,cover_table,coordinates_a_b,x,y)
+        data.discover_table(table, cover_table, x, y)
+        w = moves.gate_teleport(table, cover_table, coordinates_a_b, x, y)
         x = w[0]
         y = w[1]
         table = w[2]
-        moves.water(coordinates_water,x,y)
+        moves.water(coordinates_water, x, y)
 
-        coordinates_dolars = moves.dolars(coordinates_dolars,x,y)
+        coordinates_dolars = moves.dolars(coordinates_dolars, x, y)
 
-        data.discover_table(table,cover_table,x,y)
+        data.discover_table(table, cover_table, x, y)
         view.print_table(cover_table)
