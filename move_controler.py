9631658b7f976e0ba1b88b95b88955c9fc738f8f
PatrykMandrak <patrykmandrak123@gmail.com>
2018-10-24 11:35:17 +0200

    Added main menu screen

--- move_controler.py
+++ move_controler.py
@@ -2,22 +2,24 @@ import os
 import view
 import data_manager as data
 
+play_character = '5'
+
 def moves(table,input,x,y):
     if input == 's' and table[y+1][x] != '#':
         table[y] = data.replace_in_string(table[y],x,'.')
-        table[y+1] = data.replace_in_string(table[y+1],x,'@')
+        table[y+1] = data.replace_in_string(table[y+1],x, play_character)
         y = y + 1
     if input == 'w' and table[y-1][x] != '#':
         table[y] = data.replace_in_string(table[y],x,'.')
-        table[y-1] = data.replace_in_string(table[y-1],x,'@')
+        table[y-1] = data.replace_in_string(table[y-1],x, play_character)
         y = y - 1
     if input == 'd' and table[y][x+1] != '#':
         table[y] = data.replace_in_string(table[y],x,'.')
-        table[y] = data.replace_in_string(table[y],x+1,'@')
+        table[y] = data.replace_in_string(table[y],x+1, play_character)
         x = x + 1
     if input == 'a' and table[y][x-1] != '#':
         table[y] = data.replace_in_string(table[y],x,'.')
-        table[y] = data.replace_in_string(table[y],x-1,'@')
+        table[y] = data.replace_in_string(table[y],x-1, play_character)
         x = x - 1
     o = [table,x,y]
     return o
@@ -29,22 +31,22 @@ def teleport(table,input,x,y,cover_table):
         if input == 's' and table[y+2][x] != '#':
             table[y] = data.replace_in_string(table[y],x,'.')
             cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y+2] = data.replace_in_string(table[y+2],x,'@')
+            table[y+2] = data.replace_in_string(table[y+2],x, play_character)
             y = y + 2
         if input == 'w' and table[y-2][x] != '#':
             table[y] = data.replace_in_string(table[y],x,'.')
             cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y-2] = data.replace_in_string(table[y-2],x,'@')
+            table[y-2] = data.replace_in_string(table[y-2],x, play_character)
             y = y - 2
         if input == 'd' and table[y][x+2] != '#':
             table[y] = data.replace_in_string(table[y],x,'.')
             cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y] = data.replace_in_string(table[y],x+2,'@')
+            table[y] = data.replace_in_string(table[y],x+2, play_character)
             x = x + 2
         if input == 'a' and table[y][x-2] != '#':
             table[y] = data.replace_in_string(table[y],x,'.')
             cover_table[y] = data.replace_in_string(cover_table[y],x,'.')
-            table[y] = data.replace_in_string(table[y],x-2,'@')
+            table[y] = data.replace_in_string(table[y],x-2, play_character)
             x = x - 2
     o = [table,x,y]
     os.system("clear")
@@ -56,13 +58,13 @@ def gate_teleport(table,cover_table,coordinates_a_b,x,y):
         cover_table[y] = data.replace_in_string(cover_table[y],x,'a')
         x = coordinates_a_b[2]
         y = coordinates_a_b[3]+1
-        table[y] = data.replace_in_string(table[y],x,'@')
+        table[y] = data.replace_in_string(table[y],x, play_character)
     if coordinates_a_b[2] == x and coordinates_a_b[3] == y:
         table[y] = data.replace_in_string(table[y],x,'b')
         cover_table[y] = data.replace_in_string(cover_table[y],x,'b')
         x = coordinates_a_b[0]
         y = coordinates_a_b[1]-1
-        table[y] = data.replace_in_string(table[y],x,'@')
+        table[y] = data.replace_in_string(table[y],x, play_character)
     l = [x,y,table]
     return l
 
--- root_controler.py
+++ root_controler.py
@@ -2,37 +2,53 @@ import view
 import os
 import move_controler as moves
 import data_manager as data
+import menu
+
 
 #coordinates = [x,y]
 def run():
-    x = 3
-    y = 3
-    table = view.get_table_from_file("table.txt")
-    cover_table = view.get_empty_table(table)
-    print("Press something to start")
-    coordinates_a_b = data.find_a_b(table)
-    coordinates_water = data.find_water(table)
-    coordinates_dolars = data.find_dolars(table)
-    while True:
-        input = data.getch()
-        os.system("clear")
-        w = moves.moves(table,input,x,y)
-        table = w[0]
-        x = w[1]
-        y = w[2]
-        data.discover_table(table,cover_table,x,y)
-        w = moves.teleport(table,input,x,y,cover_table)
-        table = w[0]
-        x = w[1]
-        y = w[2]
-        data.discover_table(table,cover_table,x,y)
-        w = moves.gate_teleport(table,cover_table,coordinates_a_b,x,y)
-        x = w[0]
-        y = w[1]
-        table = w[2]
-        moves.water(coordinates_water,x,y)
+    player_choice = menu.menu()
+    if player_choice == '1':
+        os.system('clear')
+        x = 3
+        y = 3
+        table = view.get_table_from_file("table.txt")
+        cover_table = view.get_empty_table(table)
+        print("Press something to start")
+        coordinates_a_b = data.find_a_b(table)
+        coordinates_water = data.find_water(table)
+        coordinates_dolars = data.find_dolars(table)
+        while True:
+            input = data.getch()
+            os.system("clear")
+            w = moves.moves(table,input,x,y)
+            table = w[0]
+            x = w[1]
+            y = w[2]
+            data.discover_table(table,cover_table,x,y)
+            w = moves.teleport(table,input,x,y,cover_table)
+            table = w[0]
+            x = w[1]
+            y = w[2]
+            data.discover_table(table,cover_table,x,y)
+            w = moves.gate_teleport(table,cover_table,coordinates_a_b,x,y)
+            x = w[0]
+            y = w[1]
+            table = w[2]
+            moves.water(coordinates_water,x,y)
 
-        coordinates_dolars = moves.dolars(coordinates_dolars,x,y)
+            coordinates_dolars = moves.dolars(coordinates_dolars,x,y)
 
-        data.discover_table(table,cover_table,x,y)
-        view.print_table(cover_table)
+            data.discover_table(table,cover_table,x,y)
+            view.print_table(cover_table)
+
+    if player_choice == '2':
+        os.system('clear')
+        a = menu.options_menu()
+        if a == '0':
+            os.system('clear')
+            run()
+
+    if player_choice == '3':
+        os.system('clear')
+        os._exit(0)
