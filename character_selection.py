f0064bc38fe2ba2bbc1cb2d12aa1257cc777c4b1
PatrykMandrak <patrykmandrak123@gmail.com>
2018-10-24 13:11:02 +0200

    Added fullproof to character selection

--- character_selection.py
+++ character_selection.py
@@ -1,14 +1,16 @@
 import os
 
 def character_selection():
-    possible_characters = ['@', '%', '<', '>', '^', 'R', 'T', 'Y', 'u']
-    print("""
-    Possible characters: @ , % , < , > , ^ , R , T , Y , U
-    """)
-    player_character = input('\nPlease choose your character: ')
-    if player_character in possible_characters:
-        return player_character
-    else:
-        os.system('clear')
-        print("Choose character from list! ")
-        character_selection
+    impossible_character = False
+    while not impossible_character:
+        possible_characters = ['@', '%', '<', '>', '^', 'R', 'T', 'Y', 'u']
+        print("""
+        Possible characters: @ , % , < , > , ^ , R , T , Y , U
+        """)
+        player_character = input('\nPlease choose your character: ')
+        if player_character in possible_characters:
+            impossible_character = True
+            return player_character
+        else:
+            os.system('clear')
+            print("Choose character from list! ")
